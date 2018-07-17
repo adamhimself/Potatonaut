797bcf7b4f7072b9f51427cf47892eeafcc2e62f
unknown <mradamhimself@gmail.com>
2018-07-17 10:28:08 +0200

    Added dashboard controller

---
+++ Potatonaut/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Potatonaut.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddTask()
+        {
+
+        }
+    }
+}
